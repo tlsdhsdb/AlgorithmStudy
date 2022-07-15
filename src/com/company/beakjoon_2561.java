e76acd5f004ca533cb76271167ae707bfdc95f2f
onyoo <dhsdb02@gmail.com>
2022-07-15 17:16:58 +0900

    백준 2561 자바

---
+++ src/com/company/beakjoon_2561.java
@@ -0,0 +1,30 @@
+package com.company;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class beakjoon_2561 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> array = new ArrayList<>();
+
+
+        for(int i=0;i<9;i++){
+            array.add(Integer.parseInt(br.readLine()));
+        }
+        ArrayList<Integer> clone = new ArrayList<>(array);
+
+        Collections.sort(array);
+
+        int max = array.get(array.size()-1);
+
+        System.out.println(max);
+        System.out.println(clone.indexOf(max)+1);
+
+    }
+}
